518a9aefd633b391efa4e0832148d7926a289eb6
MahshidNesahat <127243129+MahshidNesahat@users.noreply.github.com>
2023-05-01 22:04:13 +0330

    Add files via upload

---
+++ Session1.py
@@ -0,0 +1,33 @@
+a = float (input("Enter First number :"))
+b = float (input("Enter Second number :"))
+c = float (input("Enter Third number :"))
+
+
+if a + b > c and a + c > b and c + b > a:
+
+    print("  triangle can be formed ")
+
+else:
+
+ print("  triangle cannot be formed ")
+
+for i in range(1,6):
+   
+    print(i*"*")
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
