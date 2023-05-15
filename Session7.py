e382ce43d3dc0145db32d0a92b3f7e99caba8f1c
MahshidNesahat <127243129+MahshidNesahat@users.noreply.github.com>
2023-05-16 00:17:56 +0330

    Add files via upload

---
+++ Session7.py
@@ -0,0 +1,25 @@
+for i in range (5,6):
+ print("#*"*i)
+ 
+ for i in  range (5,6):
+ 
+   print(i * "*#")
+ 
+ for i in  range (5,6):
+   
+      print("#*"*i)   
+
+for i in  range (5,6):
+   
+ print(i * "*#")
+      
+for i in  range (5,6):
+  
+   print("#*"*i)  
+ 
+for i in  range (5,6):
+                                 
+ print(i * "*#")
+
+
+
